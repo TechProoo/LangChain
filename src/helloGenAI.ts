7475f180dba5b5dd54d642f66fc20d140551bb28
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2025-04-15 13:32:46 +0100

    Setting up Langchain

--- src/helloGenAI.ts
+++ src/helloGenAI.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatGroq } from "@langchain/groq";
 
 dotenv.config();
 
-const llm = new ChatOpenAI();
+
+const llm = new ChatGroq ({
+  model: "llama-3.3-70b-versatile",
+})
 
 const response = await llm.invoke(
-  "Describe the importance of learning generative AI for javascript developers in 50 words."
-);
+ "Describe the importance of learning generative AI for javascript developers in 50 words"
 
-console.log(response);
+)
+
+console.log(response)
